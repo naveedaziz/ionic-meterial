1ce553ad39731670ace4c09f2d705047b78ab46d
Naveed Aziz <naveed.aziz@bramerz.pk>
2017-06-19 01:07:51 +0500

    CDA

--- public/app.routes.js
+++ public/app.routes.js
@@ -41,6 +41,6 @@ router.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
             }
         });
 
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
 
 });

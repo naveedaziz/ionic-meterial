0bf0dfe699eaeb022a2be306c8453c8a27d8dfcf
Naveed Aziz <naveed.aziz@bramerz.pk>
2017-07-06 01:15:47 +0500

    ded

--- public/app.controller.js
+++ public/app.controller.js
@@ -17,17 +17,93 @@ angular.module('appCtrl', [])
    };
 })
    .controller('appCtrl', function ($mdSidenav, $stateParams, $rootScope, $mdDialog,$state) {
-      this.events = [{
-         title: 'Event Title',
-         start: new Date(),
-         end: new Date(),
-         allDay: false
-      }];
-      console.log(this.events)
-      this.eventClicked = function(id){
-         alert(1)
-            console.log(id)
+      this.events = [
+         {
+            start: getDate(-6, 10),
+            end: getDate(-6, 11),
+            title: 'Event 1'
+         },
+         {
+            start: getDate(0, 10),
+            end: getDate(1, 11),
+            title: 'Event 1'
+         },
+         {
+            start: getDate(1, 11),
+            end: getDate(2, 12),
+            title: 'Event 2'
+         },
+         {
+            start: getDate(2, 12),
+            end: getDate(3, 13),
+            title: 'Event 3'
+         },
+         {
+            start: getDate(4, 12),
+            end: getDate(5, 13),
+            title: 'Event 4'
+         },
+         {
+            start: getDate(5, 12),
+            end: getDate(6, 13),
+            title: 'Event 5'
+         },
+         {
+            start: getDate(6, 12),
+            end: getDate(6, 13),
+            title: 'Event 6'
+         },
+         {
+            start: getDate(6, 12),
+            allDay: true,
+            title: 'Event 7'
+         },
+
+
+
+
+         {
+            start: getDate(8, 12),
+            end: getDate(8, 13),
+            title: 'Event 5'
+         },
+         {
+            start: getDate(8, 12),
+            end: getDate(8, 13),
+            title: 'Event 6'
+         },
+         {
+            start: getDate(8, 12),
+            allDay: true,
+            title: 'Event 7'
+         }
+      ];
+      this.selected = this.events[0];
+
+      this.eventClicked = function (item) {
+         console.log(item);
+      };
+
+      this.createClicked = function (date) {
+         console.log(date);
+      };
+
+      function getDate(offsetDays, hour) {
+         offsetDays = offsetDays || 0;
+         var offset = offsetDays * 24 * 60 * 60 * 1000;
+         var date = new Date(new Date().getTime() + offset);
+         if (hour) { date.setHours(hour); }
+         return date;
       }
+
+
+      this.dis = false;
+  // $timeout(function () {
+  //   $scope.events.push({
+  //     date: new Date(new Date().getTime() + 48 * 60 * 60 * 1000),
+  //     label: 'Event Three'
+  //   });
+  // }, 1000)
       this.grids = [
                      {
                         img:'student',name:'People',
